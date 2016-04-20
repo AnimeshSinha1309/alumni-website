fa9c4556328c8b5ca7461b12d3b4752a325334df
Animesh Sinha <AnimeshSinha1309@gmail.com>
2016-04-20 11:56:52 +0530

    Structure of the Listing Arrangement
    How the different batches will be listed, the top bar, the URL configuration, and a bit of the basic structure is ready.

--- AlumniWebsite/app/models.py
+++ AlumniWebsite/app/models.py
@@ -3,5 +3,32 @@ Definition of models.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Models about user and profile data.
+
+class Workplace(models.Model):
+    TYPE_CHOICES = (
+        ('SCH', 'School'),
+        ('UNV', 'University'),
+        ('BSI', 'Business'),
+        ('HOS', 'Hospital'),
+        ('OTH', 'Others'),
+    )
+    def __str__(self):
+        return self.name
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=3, choices=TYPE_CHOICES)
+
+class Alumnus(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    jobtitle = models.CharField(max_length=50)
+    workplace = models.ForeignKey(Workplace)
+    email = models.EmailField()
+    batch = models.IntegerField()
+
+# Models about event and program data
+
+class Event(models.Model):
+    name = models.CharField(max_length=50)
+    date = models.DateField()
--- AlumniWebsite/app/views.py
+++ AlumniWebsite/app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from app.models import Alumnus
 
 def home(request):
     """Renders the home page."""
@@ -15,6 +16,57 @@ def home(request):
         'app/index.html',
         {
             'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def events(request):
+    """Renders the events page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/events.html',
+        {
+            'title':'Events Calender',
+            'year':datetime.now().year,
+        }
+    )
+
+def alumni_batches(request):
+    """Renders the alumni batch listing."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/alumni_batches.html',
+        {
+            'title':'Alumni List',
+            'batches':range(2000, 2017),
+            'year':datetime.now().year,
+        }
+    )
+
+def alumni_batchlist(request, batch):
+    """Renders the alumni batch listing."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/alumni_batchlist.html',
+        {
+            'title':'Alumni List',
+            'batch':batch,
+            'people':Alumnus.objects.filter(batch=int(batch)),
+            'year':datetime.now().year,
+        }
+    )
+
+def contribute(request):
+    """Renders the contribute page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contribute.html',
+        {
+            'title':'Contribute to the School',
             'year':datetime.now().year,
         }
     )
@@ -32,15 +84,14 @@ def contact(request):
         }
     )
 
-def about(request):
-    """Renders the about page."""
+def school(request):
+    """Renders the school page."""
     assert isinstance(request, HttpRequest)
     return render(
         request,
-        'app/about.html',
+        'app/school.html',
         {
-            'title':'About',
-            'message':'Your application description page.',
+            'title':'St. Thomas Today',
             'year':datetime.now().year,
         }
     )
